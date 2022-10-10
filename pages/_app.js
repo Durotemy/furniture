559ae4e0c004e932b71b0ba191337b8ce32c22d3
durotemy <durotemy@gmail.com>
2022-10-10 14:14:39 +0100

    navbar created

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import '../styles/globals.css';
-import Head from "next/head";
-import NProgress from "nprgress"
-import { ChakraProvider } from "@chakra-ui/react";
-
+ import NProgress from "nprogress"
 import Layout from "../components/Layout";
-
+import '../styles/globals.css'
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
-
-export default MyApp
+  return (
+    <div>
+      <Head>
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </div>
+  )
+}
+export default MyApp;
