9de8bfbf001aa49a5d35c894a6ab6875a470a55e
durotemy <durotemy@gmail.com>
2022-10-10 15:46:14 +0100

    props in search page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+import Property from "../components/Property";
+import SearchFilters from "./SearchFilters";
+import noresult from "../assets/images/noresult.svg";
+
+const SearchFilters = () => {
+  return <div>Shows</div>;
+};
+export default SearchFilters;
